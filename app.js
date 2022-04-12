3e76eeb15c0e614b870b67e373004c2cf039ae39
hfang821 <95199209+hfang821@users.noreply.github.com>
2022-04-12 18:02:27 -0400

    added the functionality of generating html file

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
-//const means the defined variable can't be reassigned a new value.
-const profileDataArgs = process.argv.slice(2,process.argv.length);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
+//const means the defined variable can't be reassigned a new value.
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs; 
+
+fs.writeFile('./index.html', generatePage(name,github), err => {
+    if (err) throw new Error(err);
+
+    console.log('Portfolio complete! check out index.html to see the output!');
+});
+
+
+
+
+/*
 console.log(profileDataArgs);
 
 const printProfileData = profileDataArr => {
@@ -21,5 +36,9 @@ const printProfileData = profileDataArr => {
 
 printProfileData(profileDataArgs);
 
+*/
+
 //let: block-scoped (will not be redefined in the global if defined in a block)
 //var: function-scoped (will be redefined in the global)
+
+
