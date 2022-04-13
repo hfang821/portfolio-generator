baf0ed34a2c835481ea7f429265f3de6875c563b
hfang821 <95199209+hfang821@users.noreply.github.com>
2022-04-13 13:23:44 -0400

    first inquirer question set

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
 
-//const means the defined variable can't be reassigned a new value.
-const profileDataArgs = process.argv.slice(2);
+inquirer
+  .prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+      }
+  ])
+  .then((answers) => {
+    console.log(answers);
+  })
 
-const [name, github] = profileDataArgs; 
 
-fs.writeFile('./index.html', generatePage(name,github), err => {
-    if (err) throw new Error(err);
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-    console.log('Portfolio complete! check out index.html to see the output!');
-});
+// //const means the defined variable can't be reassigned a new value.
+// const profileDataArgs = process.argv.slice(2);
+
+// const [name, github] = profileDataArgs; 
+
+// fs.writeFile('./index.html', generatePage(name,github), err => {
+//     if (err) throw new Error(err);
+
+//     console.log('Portfolio complete! check out index.html to see the output!');
+// });
 
 
 
